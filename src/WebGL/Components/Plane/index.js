02292ec40f8421dce03727569256bdbe9ff0999d
FlanB <clement.renou@mmibordeaux.com>
2023-10-20 12:13:44 +0200

    style: apply prettier --write script

--- src/WebGL/Components/Plane/index.js
+++ src/WebGL/Components/Plane/index.js
@@ -1,40 +1,37 @@
-import Experience from "webgl/Experience.js";
-import fragmentShader from "./fragmentShader.frag";
-import vertexShader from "./vertexShader.vert";
-import { Mesh, PlaneGeometry, ShaderMaterial, Vector3} from "three";
+import Experience from 'webgl/Experience.js'
+import fragmentShader from './fragmentShader.frag'
+import vertexShader from './vertexShader.vert'
+import { Mesh, PlaneGeometry, ShaderMaterial, Vector3 } from 'three'
 
 export default class Plane {
-    constructor(_position = new Vector3(0, 0, 0)) {
-        this.experience = new Experience();
-        this.scene = this.experience.scene;
-        this.debug = this.experience.debug;
+	constructor(_position = new Vector3(0, 0, 0)) {
+		this.experience = new Experience()
+		this.scene = this.experience.scene
+		this.debug = this.experience.debug
 
-        this.position = _position;
+		this.position = _position
 
-        this.setGeometry();
-        this.setMaterial();
-        this.setMesh();
-    }
+		this.setGeometry()
+		this.setMaterial()
+		this.setMesh()
+	}
 
-    setGeometry() {
-        this.geometry = new PlaneGeometry(2, 2, 1);
-    }
+	setGeometry() {
+		this.geometry = new PlaneGeometry(2, 2, 1)
+	}
 
-    setMaterial() {
-        this.material = new ShaderMaterial({
-            fragmentShader,
-            vertexShader,
-        });
-    }
+	setMaterial() {
+		this.material = new ShaderMaterial({
+			fragmentShader,
+			vertexShader,
+		})
+	}
 
-    setMesh() {
-        this.mesh = new Mesh(this.geometry, this.material);
-        this.mesh.position.copy(this.position);
-        this.mesh.name = "plane";
-        this.scene.add(this.mesh);
-
-    }
-    update() {
-
-    }
+	setMesh() {
+		this.mesh = new Mesh(this.geometry, this.material)
+		this.mesh.position.copy(this.position)
+		this.mesh.name = 'plane'
+		this.scene.add(this.mesh)
+	}
+	update() {}
 }
--- src/WebGL/Scenes/Main.js
+++ src/WebGL/Scenes/Main.js
@@ -1,20 +1,20 @@
-import Experience from "../Experience.js";
-import Plane from "components/Plane/index.js";
+import Experience from '../Experience.js'
+import Plane from 'components/Plane/index.js'
 
 export default class Main {
-  constructor() {
-    this.experience = new Experience();
-    this.scene = this.experience.scene;
-    this.resources = this.experience.resources;
+	constructor() {
+		this.experience = new Experience()
+		this.scene = this.experience.scene
+		this.resources = this.experience.resources
 
-    // Wait for resources
-    this.resources.on("ready", () => {
-      // Setup
-      this.plane = new Plane();
-    });
-  }
+		// Wait for resources
+		this.resources.on('ready', () => {
+			// Setup
+			this.plane = new Plane()
+		})
+	}
 
-  update() {
-   if (this.plane) this.plane.update();
-  }
+	update() {
+		if (this.plane) this.plane.update()
+	}
 }
